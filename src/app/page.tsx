0aedbfc6a5226b11fdee7c270fe633a8c2837aeb
shajahan1299 <shajahancse1299@gmail.com>
2025-01-27 12:16:14 +0530

    frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from './page.module.css'
-import dynamic from 'next/dynamic'
+import HomeSlider from '@/components/HomeSlider/HomeSlider';
+
+//import dynamic from 'next/dynamic'
 import MovieCarousel from '@/components/moviecarousel/MovieCarousel'
-const HomeSlider = dynamic(() => import('@/components/HomeSlider/HomeSlider'), { ssr: false });
+//const HomeSlider = dynamic(() => import('@/components/HomeSlider/HomeSlider'), { ssr: false });
 
 export default function Home() {
 
